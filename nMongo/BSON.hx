a27fac2474926b4af6400454975d93619f4ba26a
Philipp Klose <me@thehippo.de>
2010-04-24 18:06:07 +0200

    added BSON wrapper class

---
+++ nMongo/BSON.hx
@@ -0,0 +1,21 @@
+package nMongo;
+
+
+class BSON {
+
+	var data:Void;
+
+	static var _decode = neko.Lib.load("nmongo","n_bson_decode",1);
+	static var _encode = neko.Lib.load("nmongo","n_bson_encode",1);
+	
+	public inline function decode<T>():T
+		return neko.Lib.nekoToHaxe(BSON._decode(this.data))
+	
+	public static inline function encode<T>(object:T):BSON
+		return new BSON(BSON._encode(neko.Lib.haxeToNeko(object)))
+	
+	private function new(data:Void)
+		this.data = data
+
+
+}
